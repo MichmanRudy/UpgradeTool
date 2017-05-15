c05bf6a39288fc26b4ceb0f6dfea8042d78319ac
mrudyk-softheme <mikhail.rudik@softheme.com>
2017-05-15 04:48:02 -0700

    added link to disableSMBV1Menu.php from result page

--- htdocs/disableSMBV1.php
+++ htdocs/disableSMBV1.php
@@ -4,7 +4,7 @@ set_time_limit(300);
 $scriptName = $_SERVER['DOCUMENT_ROOT'].'/PS/disableSMBV1.ps1';
 $fileName='/disableSMBV1_'.date('m-d-Y_H.i.s').'_'.$_POST['IP'].'.php';
 $logName=$_SERVER['DOCUMENT_ROOT'].'/logs/PS'.$fileName ;
-$logSource=$_SERVER['DOCUMENT_ROOT'].'/upgradeResult.php';
+$logSource=$_SERVER['DOCUMENT_ROOT'].'/disableSMBV1Result.php';
 copy($logSource,$logName);
 
 exec('PowerShell  -file "'.$scriptName.'" -IP "'.$_POST['IP'].'" -password "'.$_POST['password'].'" -user "'.$_POST['user'].'" -logName  "'.$logName.'" ');
